92bea7074432b8e7f3cd9d7c283000775c9c919b
Gautam Pachnanda <gautam.pachnanda@digital.homeoffice.gov.uk>
2021-02-16 18:39:30 +0000

    Removing debug line

--- bin/runner/run.sh
+++ bin/runner/run.sh
@@ -2,7 +2,6 @@
 [[ -z "${DEBUG}" ]] || set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source /drone/src/bin/util.sh || exit 1
-ls -lah /drone/src/bin/runner
 source /drone/src/bin/runner/env.sh || exit 1
 
 info "---"
