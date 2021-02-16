39de9ac328a1c12d763d23bc2e6e9d82ec657c7c
Gautam Pachnanda <gautam.pachnanda@digital.homeoffice.gov.uk>
2021-02-16 17:44:49 +0000

    Removed call to env.sh

--- bin/runner/env.sh
+++ bin/runner/env.sh
@@ -2,7 +2,6 @@
 [[ -z "${DEBUG}" ]] || set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source /drone/src/bin/util.sh || exit 1
-source ${DIR}/env.sh || exit 1
 
 info "---"
 info "Kube Environment: ${DRONE_DEPLOY_TO}"
