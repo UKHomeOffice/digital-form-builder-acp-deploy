734999262693e0d384ec1476a6cef780627c799b
kakani sridhar <sreedhara.kakani@yahoo.co.uk>
2021-02-24 13:50:17 +0000

    correcting namespace and urls

--- bin/runner/env.sh
+++ bin/runner/env.sh
@@ -5,8 +5,8 @@ case ${DRONE_DEPLOY_TO} in
 'prod')
     export KUBE_SERVER="https://kube-api-prod.prod.acp.homeoffice.gov.uk"
     export KUBE_TOKEN=${KUBE_TOKEN_ACP_PROD=notset}
-    export INTERNAL_URL=".internal.stp-prod.homeoffice.gov.uk"
-    export EXTERNAL_URL=".stp-prod.homeoffice.gov.uk"
+    export INTERNAL_URL=".internal.stp-forms-prod.homeoffice.gov.uk"
+    export EXTERNAL_URL=".stp-forms-prod.homeoffice.gov.uk"
     export SERVICE_REPLICAS=1
     export UPTIME='Mon-Sun 00:00-00:00 Europe/London'
     export KUBE_CERTIFICATE_AUTHORITY=https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-prod.crt
@@ -17,8 +17,8 @@ case ${DRONE_DEPLOY_TO} in
     export KUBE_SERVER="https://kube-api-notprod.notprod.acp.homeoffice.gov.uk"
     export KUBE_TOKEN=${KUBE_TOKEN_ACP_NOTPROD=notset}
     export UPTIME='Mon-Fri 08:00-22:00 Europe/London'
-    export INTERNAL_URL=".dev.internal.stp-notprod.homeoffice.gov.uk"
-    export EXTERNAL_URL=".dev.stp-notprod.homeoffice.gov.uk"
+    export INTERNAL_URL=".dev.internal.stp-forms-notprod.homeoffice.gov.uk"
+    export EXTERNAL_URL=".dev.stp-forms-notprod.homeoffice.gov.uk"
     export SERVICE_REPLICAS=1
     export KUBE_NAMESPACE="stp-forms-dev"
     export KUBE_CERTIFICATE_AUTHORITY=https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt
@@ -28,8 +28,8 @@ case ${DRONE_DEPLOY_TO} in
     export KUBE_SERVER="https://kube-api-notprod.notprod.acp.homeoffice.gov.uk"
     export KUBE_TOKEN=${KUBE_TOKEN_ACP_NOTPROD=notset}
     export UPTIME='Mon-Fri 08:00-22:00 Europe/London'
-    export INTERNAL_URL=".test.internal.stp-notprod.homeoffice.gov.uk"
-    export EXTERNAL_URL=".test.stp-notprod.homeoffice.gov.uk"
+    export INTERNAL_URL=".test.internal.stp-forms-notprod.homeoffice.gov.uk"
+    export EXTERNAL_URL=".test.stp-forms-notprod.homeoffice.gov.uk"
     export SERVICE_REPLICAS=1
     export KUBE_NAMESPACE="stp-forms-test"
     export KUBE_CERTIFICATE_AUTHORITY=https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt
@@ -39,8 +39,8 @@ case ${DRONE_DEPLOY_TO} in
     export KUBE_SERVER="https://kube-api-notprod.notprod.acp.homeoffice.gov.uk"
     export KUBE_TOKEN=${KUBE_TOKEN_ACP_NOTPROD=notset}
     export UPTIME='Mon-Fri 08:00-22:00 Europe/London'
-    export INTERNAL_URL=".preprod.internal.stp-notprod.homeoffice.gov.uk"
-    export EXTERNAL_URL=".preprod.stp-notprod.homeoffice.gov.uk"
+    export INTERNAL_URL=".preprod.internal.stp-forms-notprod.homeoffice.gov.uk"
+    export EXTERNAL_URL=".preprod.stp-forms-notprod.homeoffice.gov.uk"
     export SERVICE_REPLICAS=1
     export KUBE_NAMESPACE="stp-forms-preprod"
     export KUBE_CERTIFICATE_AUTHORITY=https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt
