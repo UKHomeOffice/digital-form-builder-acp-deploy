917fd30a03b14da9fb471fc08945c759cea9bca7
kakani sridhar <sreedhara.kakani@yahoo.co.uk>
2021-03-04 16:55:08 +0000

    printing file contents

--- bin/get-app-url.sh
+++ bin/get-app-url.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env sh
 [[ -z "${DEBUG}" ]] || set -x
 
-if [ ${DRONE_DEPLOY_TO} == 'test' ]
-then
-   export APP_URL=https://designer.test.stp-forms-notprod.homeoffice.gov.uk
-else
-   export APP_URL=https://designer.dev.stp-forms-notprod.homeoffice.gov.uk
-   
-fi
+case "$DRONE_DEPLOY_TO" in
+ test) export APP_URL=https://designer.test.stp-forms-notprod.homeoffice.gov.uk ;;
+    *) export APP_URL=https://designer.dev.stp-forms-notprod.homeoffice.gov.uk ;;
+esac
 
+echo ${APP_URL}
 cd digital-form-builder/smoke-tests/designer/
 sed -i 's|http://localhost:3000|${APP_URL}|g' wdio.headless.conf.js
 
+head -5 wdio.headless.conf.js
