55d803ccc8cc0fa187c2e9b1e46dc126f6a88b8e
kakani sridhar <sreedhara.kakani@yahoo.co.uk>
2021-03-04 14:55:29 +0000

    chaning smoke test url

---
+++ bin/get-app-url.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+case ${DRONE_DEPLOY_TO} in
+
+'development')
+    export APP_URL=https://designer.dev.stp-forms-notprod.homeoffice.gov.uk
+        ;;
+      
+'test')
+    export APP_URL=https://designer.test.stp-forms-notprod.homeoffice.gov.uk
+        ;;
+;;
+esac
+cd digital-form-builder/smoke-tests/designer/
+sed -i 's|http://localhost:3000|${APP_URL}|g' wdio.headless.conf.js
+
--- bin/get-latest-tag.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd digital-form-builder
-git fetch --all --tags
-latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-export TAG=${DESIGNER_TAG=${latest_tag}}
-echo "TAG=$TAG"
