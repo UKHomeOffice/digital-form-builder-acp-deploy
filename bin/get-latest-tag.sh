22acaa05f6132a04200b8abc30deb723e6550366
kakani sridhar <sreedhara.kakani@yahoo.co.uk>
2021-03-04 14:15:01 +0000

    moving cd to shell

--- bin/get-latest-tag.sh
+++ bin/get-latest-tag.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+cd digital-form-builder
 git fetch --all --tags
 latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 export TAG=${DESIGNER_TAG=${latest_tag}}
